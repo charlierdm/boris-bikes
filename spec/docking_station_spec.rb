e36a4aa8928791ab356047aa6ee44a4bf4136e05
charlierdm <charlie.davismiller@gmail.com>
2021-02-03 17:39:23 +0000

    Day 2 Boris Bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,6 +7,9 @@ describe DockingStation do
     end
   end
 
+  it 'responds to docking bike' do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
 
   it 'responds to release_bike' do
     expect(subject).to respond_to :release_bike
@@ -19,4 +22,15 @@ describe DockingStation do
    it 'returns a working bike' do
      expect(subject.release_bike.working?).to be true
    end
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 end
