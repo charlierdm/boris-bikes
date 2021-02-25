8dfdff4431b8bfae5f6628721003f4838a2de5e9
charlierdm <charlie.davismiller@gmail.com>
2021-02-25 04:55:10 +0000

    garage edited

--- lib/garage.rb
+++ lib/garage.rb
@@ -1,7 +1,19 @@
 class Garage
+  DEFAULT_CAPACITY = 50
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @storage = []
+    @capacity = capacity
+  end
 
   def fix_bike(bike)
     @broken = false
+  end
+
+  private
+
+  def full
+    storage.count >= DEFAULT_CAPACITY
   end
 
 end
